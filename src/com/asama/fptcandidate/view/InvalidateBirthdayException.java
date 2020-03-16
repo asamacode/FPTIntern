c695dfba864b98c23a4f752a2a940d4909455c35
asamacode <trhuuluong@gmail.com>
2020-03-16 15:59:54 +0700

    add load candidate

---
+++ src/com/asama/fptcandidate/view/InvalidateBirthdayException.java
@@ -0,0 +1,13 @@
+package com.asama.fptcandidate.view;
+
+public class InvalidateBirthdayException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidateBirthdayException() {
+		super("Invalidate type of candidate birthday");
+	}
+}
---
+++ src/com/asama/fptcandidate/view/InvalidateEmailException.java
@@ -0,0 +1,12 @@
+package com.asama.fptcandidate.view;
+
+public class InvalidateEmailException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidateEmailException() {
+		super("Invalidate type of candidate email");
+	}
+}
